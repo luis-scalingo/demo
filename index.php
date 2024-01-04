5cc0c49c37c40eda597743d9612e8d699c098fc4
luis-scalingo <151912804+luis-scalingo@users.noreply.github.com>
2024-01-04 14:42:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>Scalingo Demo</title>
     </head>
     <body>
-        <?php echo '<h2>Hello from Scalingo!!</h2>'; ?>
+        <?php echo '<h2>Hello François from Scalingo!!</h2>'; ?>
     </body>
 </html>
