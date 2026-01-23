8cbaac8157c0b14ec66fd1e5ea92ad2e45fca2cf
luis-scalingo <151912804+luis-scalingo@users.noreply.github.com>
2026-01-23 09:36:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>Scalingo Demo</title>
     </head>
     <body>
-        <?php echo '<h2>Hello Min Int from Scalingo!!</h2>'; ?>
+        <?php echo '<h2>Hello from Scalingo!!</h2>'; ?>
     </body>
 </html>
